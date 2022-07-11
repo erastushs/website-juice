c7a204f6cfecbe66834382d37473f8105af5c5c2
Erastus-HS <erastushs@gmail.com>
2022-07-11 17:06:29 +0900

    menambahkan pop up scroll up

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//
+// membuat back navbar tidak transparan saat sudah di scroll
 
 window.addEventListener("scroll", function () {
   const nav = document.querySelector("nav");
@@ -11,4 +11,15 @@ const ul = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
   ul.classList.toggle("slide");
+});
+
+// scroll up pop up
+let offset = 0;
+window.addEventListener("scroll", function () {
+  let st = window.pageYOffset;
+  if (st > offset) {
+    document.querySelector(".fa-arrow-up").classList.add("active");
+  } else {
+    document.querySelector(".fa-arrow-up").classList.remove("active");
+  }
 });
